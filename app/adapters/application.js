87857d9367d6039173084e500913f76989287286
Adam Skoczylas <skocadam@gmail.com>
2019-01-20 14:26:23 -0500

    changed giphy host to https

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,6 @@ import DS from "ember-data";
 
 export default DS.RESTAdapter.extend({
   api_key: "CJJlnuT0vAt2j733vIkxgYGJbLN0OrZf",
-  host: "http://api.giphy.com",
+  host: "https://api.giphy.com",
   namespace: "v1/gifs"
 });

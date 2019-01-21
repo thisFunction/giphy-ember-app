ace838b4363fbb6c2170bd1056a7b717807fd56b
Adam Skoczylas <skocadam@gmail.com>
2019-01-21 09:45:43 -0500

    add sorting tests

--- tests/integration/components/giphy-minature-favorites/component-test.js
+++ tests/integration/components/giphy-minature-favorites/component-test.js
@@ -1,6 +1,6 @@
 import {module, test} from "qunit";
 import {setupRenderingTest} from "ember-qunit";
-import {render} from "@ember/test-helpers";
+import {render, click} from "@ember/test-helpers";
 import hbs from "htmlbars-inline-precompile";
 
 const model = [
@@ -11,8 +11,26 @@ const model = [
     giphyId: "1000"
   },
   {
-    id: 3,
+    id: 2,
     userRating: 1,
+    imageUrl: "https://media1.giphy.com/media/l0Iym8uibS5p0AJdC/200.gif",
+    giphyId: "1000"
+  },
+  {
+    id: 3,
+    userRating: 4,
+    imageUrl: "https://media1.giphy.com/media/xUOwG5aFxxcLTVCaeQ/200.gif",
+    giphyId: "1000"
+  },
+  {
+    id: 4,
+    userRating: 5,
+    imageUrl: "https://media1.giphy.com/media/l0Iym8uibS5p0AJdC/200.gif",
+    giphyId: "1000"
+  },
+  {
+    id: 5,
+    userRating: 2,
     imageUrl: "https://media1.giphy.com/media/l0Iym8uibS5p0AJdC/200.gif",
     giphyId: "1000"
   }
@@ -27,7 +45,7 @@ module("Integration | Component | giphy-minature-favorites", function(hooks) {
 
   test("it renders 3 sorting buttons with correct css classes", async function(assert) {
     await render(hbs`{{giphy-minature-favorites favorites=model}}`);
-    assert.equal(this.element.querySelectorAll(".small").length, 3, "three sorting buttons render");
+    assert.equal(this.element.querySelectorAll(".small").length, 3, "3 sorting buttons render");
   });
 
   test("it renders correct sorting button icon classes", async function(assert) {
@@ -37,11 +55,11 @@ module("Integration | Component | giphy-minature-favorites", function(hooks) {
     assert.ok(this.element.querySelector(".small:nth-child(3)").firstElementChild.className.includes('fa-arrow-circle-o-down') , "first button renders font-awesome arrow circle up");
   });
 
-  test("it renders 2 giphy minature boxes", async function(assert) {
+  test("it renders 5 giphy minature boxes", async function(assert) {
     await render(hbs`{{giphy-minature-favorites favorites=model}}`);
-    assert.equal(this.element.querySelectorAll(".giphy-minature-rating").length, 2, "two minature boxes render");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating").length, 5, "5 minature boxes render");
   });
-
+  
   test("it renders correct amount of stars for giphy minature 1", async function(assert) {
     await render(hbs`{{giphy-minature-favorites favorites=model}}`);
     assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(1) .fa-star").length, 3, "3 filled stars for giphy minature 1 render");
@@ -54,5 +72,68 @@ module("Integration | Component | giphy-minature-favorites", function(hooks) {
     assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(2) .fa-star").length, 1, "1 filled stars for giphy minature 1 render");
     assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(2) .fa-star-o").length, 4, "4 empty stars for giphy minature 1 render");
 
+  });
+
+  test("it renders giphy minatures in correct order when sorting ascending", async function(assert) {
+    await render(hbs`{{giphy-minature-favorites favorites=model}}`);
+
+    await click('button:nth-of-type(1)');   
+
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(1) .fa-star").length, 1, "1 filled stars for giphy minature with rating 1");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(1) .fa-star-o").length, 4, "4 empty stars for giphy minature with rating 1");
+  
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(2) .fa-star").length, 2, "2 filled stars for giphy minature with rating 2");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(2) .fa-star-o").length, 3, "3 empty stars for giphy minature with rating 3");
+    
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(3) .fa-star").length, 3, "3 filled stars for giphy minature with rating 3");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(3) .fa-star-o").length, 2, "2 empty stars for giphy minature with rating 3");
+
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(4) .fa-star").length, 4, "5 filled stars for giphy minature with rating 4");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(4) .fa-star-o").length, 1, "2 empty stars for giphy minature with rating 4");
+  
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(5) .fa-star").length, 5, "5 filled stars for giphy minature with rating 5");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(5) .fa-star-o").length, 0, "0 empty stars for giphy minature with rating 5");
+  });
+
+  test("it renders giphy minatures in correct order when sorting descending", async function(assert) {
+    await render(hbs`{{giphy-minature-favorites favorites=model}}`);
+
+    await click('button:nth-of-type(3)');   
+  
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(1) .fa-star").length, 5, "5 filled stars for giphy minature with rating 5");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(1) .fa-star-o").length, 0, "0 empty stars for giphy minature with rating 5");
+    
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(2) .fa-star").length, 4, "5 filled stars for giphy minature with rating 4");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(2) .fa-star-o").length, 1, "2 empty stars for giphy minature with rating 4");
+    
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(3) .fa-star").length, 3, "3 filled stars for giphy minature with rating 3");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(3) .fa-star-o").length, 2, "2 empty stars for giphy minature with rating 3");
+    
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(4) .fa-star").length, 2, "2 filled stars for giphy minature with rating 2");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(4) .fa-star-o").length, 3, "3 empty stars for giphy minature with rating 3");
+    
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(5) .fa-star").length, 1, "1 filled stars for giphy minature with rating 1");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(5) .fa-star-o").length, 4, "4 empty stars for giphy minature with rating 1");
+  });
+
+  
+  test("it renders giphy minatures in correct order when sorting default", async function(assert) {
+    await render(hbs`{{giphy-minature-favorites favorites=model}}`);
+    await click('button:nth-of-type(2)');   
+  
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(1) .fa-star").length, model[0].userRating, "3 filled stars for giphy minature with rating 3");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(1) .fa-star-o").length, 5 - model[0].userRating, "2 empty stars for giphy minature with rating 3");
+
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(2) .fa-star").length, model[1].userRating, "1 filled stars for giphy minature with rating 1");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(2) .fa-star-o").length, 5 - model[1].userRating, "4 empty stars for giphy minature with rating 1");
+    
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(3) .fa-star").length, model[2].userRating, "4 filled stars for giphy minature with rating 4");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(3) .fa-star-o").length, 5 - model[2].userRating, "1 empty stars for giphy minature with rating 4");
+    
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(4) .fa-star").length, model[3].userRating, "5 filled stars for giphy minature with rating 5");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(4) .fa-star-o").length, 5 - model[3].userRating, "0 empty stars for giphy minature with rating 5");
+    
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(5) .fa-star").length, model[4].userRating, "2 filled stars for giphy minature with rating 2");
+    assert.equal(this.element.querySelectorAll(".giphy-minature-rating:nth-child(5) .fa-star-o").length, 5 - model[4].userRating, "4 empty stars for giphy minature with rating 2");
   });
 });

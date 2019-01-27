5c04064c60bba606b4d9cff272074e98386ed476
Adam Skoczylas <skocadam@gmail.com>
2019-01-27 13:54:47 -0500

    begin move

--- app/components/pagination-section/component.js
+++ app/components/pagination-section/component.js
@@ -1,50 +1,12 @@
 import Component from "@ember/component";
-import {inject as service} from "@ember/service";
-import {computed, get} from "@ember/object";
-import {alias} from "@ember/object/computed";
 
 export default Component.extend({
-  router: service(),
-  itemsPerPage: computed("pagination.count", function() {
-    return Number(get(this, "pagination.count"));
-  }),
-  offset: computed("router.currentRoute.queryParams.offset", function() {
-    return Number(get(this, "router.currentRoute.queryParams.offset"));
-  }),
-  totalItemCount: alias("pagination.totalCount"),
-  searchParams: alias("router.currentRoute.queryParams.q"),
-  previousButtonDisabled: computed("router.currentRoute.queryParams.offset", "itemsPerPage", function() {
-    const offset = get(this, "offset");
-    const itemsPerPage = get(this, "itemsPerPage");
-    return !(offset - itemsPerPage >= 0);
-  }),
-  nextButtonDisabled: computed("router.currentRoute.queryParams.offset", "itemsPerPage", "pagination.totalCount", function() {
-    const offset = get(this, "offset");
-    const itemsPerPage = get(this, "itemsPerPage");
-    const totalItemCount = Number(get(this, "pagination.totalCount"));
-    return offset + itemsPerPage >= totalItemCount;
-  }),
-  transitionToPage(offset) {
-    const searchParams = get(this, "searchParams");
-    this.get("router").transitionTo("index.gifs", {
-      queryParams: {
-        q: `${searchParams}`,
-        offset: offset
-      }
-    });
-  },
   actions: {
     next() {
-      const nextPageOffset =
-        Number(get(this, "router.currentRoute.queryParams.offset")) +
-        get(this, "itemsPerPage");
-      this.transitionToPage(nextPageOffset);
+      this.nextPage();
     },
     previous() {
-      const offset = Number(get(this, "router.currentRoute.queryParams.offset"));
-      const itemsPerPage = get(this, "itemsPerPage");
-      const previousPageOffset = offset - itemsPerPage;
-      this.transitionToPage(previousPageOffset);
+      this.previousPage();
     }
   }
 });
--- app/index/gifs/controller.js
+++ app/index/gifs/controller.js
@@ -1,13 +1,56 @@
 import Controller from "@ember/controller";
-import { computed, get } from "@ember/object"
-import { inject } from '@ember/service';
+import {computed, get} from "@ember/object";
+import {inject} from "@ember/service";
 
 export default Controller.extend({
-  store:  inject(),
-  q: null,  
+  store: inject(),
+  q: null,
   offset: null,
-  pagination: computed('store.offset', 'model.query.offset', function() {
-    const offset = get(this, 'model.query.offset');
-    return this.get('store').peekRecord('pagination', offset)
-  })
+  pagination: computed("store.offset", "model.query.offset", function() {
+    const offset = get(this, "model.query.offset");
+    return this.get("store").peekRecord("pagination", offset);
+  }),
+
+
+  /// From pagination Component
+
+  itemsPerPage: computed("pagination.count", function() {
+    return Number(get(this, "pagination.count"));
+  }),
+  offset: computed("router.currentRoute.queryParams.offset", function() {
+    return Number(get(this, "router.currentRoute.queryParams.offset"));
+  }),
+  totalItemCount: alias("pagination.totalCount"),
+  searchParams: alias("router.currentRoute.queryParams.q"),
+  previousButtonDisabled: computed("router.currentRoute.queryParams.offset", "itemsPerPage", function() {
+    const offset = get(this, "offset");
+    const itemsPerPage = get(this, "itemsPerPage");
+    return !(offset - itemsPerPage >= 0);
+  }),
+  nextButtonDisabled: computed("router.currentRoute.queryParams.offset", "itemsPerPage", "pagination.totalCount", function() {
+    const offset = get(this, "offset");
+    const itemsPerPage = get(this, "itemsPerPage");
+    const totalItemCount = Number(get(this, "pagination.totalCount"));
+    return offset + itemsPerPage >= totalItemCount;
+  }),
+  transitionToPage(offset) {
+    const searchParams = get(this, "searchParams");
+    this.get("router").transitionTo("index.gifs", {
+      queryParams: {
+        q: `${searchParams}`,
+        offset: offset
+      }
+    });
+  },
+
+  ///
+  actions: {
+    nextPage() {
+      debugger
+    },
+
+    previousPage() {
+      debugger
+    }
+  }
 });
